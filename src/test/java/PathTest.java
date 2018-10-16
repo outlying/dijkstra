fc93e6d36f0b8aaf27bb5c4e89b440f8bea3e01b
Iwo Polański <outlying@gmail.com>
2018-10-16 20:14:12 +0200

    tests added

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Nie wysyłać do końcowego projektu !!!
@@ -39,12 +38,44 @@ public class PathTest {
     }
 
     /**
+     * Graf:
+     * <p>
+     *
+     * <code>
+     * [1]--(2)--[2]
+     * |       / |
+     * |      /  |
+     * (5) (2)   (10)
+     * |  /      |
+     * | /       |
+     * [3]--(2)--[4]
+     * </code>
+     */
+    @Test
+    public void moreComplex() {
+        Main.Graph graph = graph(
+                triple(1, 2, 2f),
+                triple(1, 3, 5f),
+                triple(2, 3, 2f),
+                triple(3, 4, 2f),
+                triple(2, 4, 10f));
+
+        List<Result> correctResults = correctResults(
+                indexResult(2, 0.5f),
+                indexResult(3, 0.2f),
+                indexResult(4, 1.5f));
+
+        verify(graph, correctResults);
+    }
+
+    /**
      * Weryfikuje graf
      */
-    private void verify(Main.Graph graph, List<Result> results){
+    private void verify(Main.Graph graph, List<Result> results) {
         Path path = factoryPath(graph);
 
-        for (Result result: results) {
+        for (Result result : results) {
+            System.out.println("Testing for index " + result.endPointIndex);
             Float calculatedCost = path.findShortestCost(1, result.endPointIndex);
             Float correctCost = result.cost;
             assertEquals(calculatedCost, correctCost);
@@ -54,7 +85,7 @@ public class PathTest {
     /**
      * Budowanie klasy liczącej
      */
-    private Path factoryPath(Main.Graph graph){
+    private Path factoryPath(Main.Graph graph) {
         return new Path(graph);
     }
 
